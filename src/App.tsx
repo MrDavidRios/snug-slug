81af385f9311ddff9ccba86793fcbb80b86c6a24
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-11-14 10:35:23 -0500

    feat: starter button component added

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,14 @@ import { Button } from "./components/button/Button";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [otherCount, setOtherCount] = useState(0);
-
-  let otherOtherCount = 0;
 
   return (
     <>
       <h1>Snug Slug</h1>
       <div className="card">
         <p>{`Count is ${count}`}</p>
-        <p>{`Other count is ${otherCount}`}</p>
-        <p>{`Other other count is ${otherOtherCount}`}</p>
         <br />
         <Button onClick={() => setCount((count) => count + 1)} text="Click me!" />
-        <Button onClick={() => setOtherCount((count) => count + 1)} text="Click me too!" />
-        <Button onClick={() => otherOtherCount++} text="Click me three!" />
       </div>
     </>
   );
