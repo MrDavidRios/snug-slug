e8a594bca61835ac5ee07d82f911b974fcb7c6b0
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-11-21 12:53:39 -0500

    fix: some renamings

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SnugSlugLogo from "../../assets/SnugSlugLogo.png";
 import { Size } from "../../types/size";
 
 interface HeroProps {
+  redirectToHome?: boolean;
   size?: Size;
 }
 
-export const Hero: React.FC<HeroProps> = ({ size = Size.Large }) => {
+export const Hero: React.FC<HeroProps> = ({ redirectToHome = false, size = Size.Large }) => {
   return (
     <div className={`hero size-${size}`}>
-      <img src={SnugSlugLogo} alt="Logo" />
+      {redirectToHome ? (
+        <Link to="/">
+          <img src={SnugSlugLogo} alt="Logo" />
+        </Link>
+      ) : (
+        <img src={SnugSlugLogo} alt="Logo" />
+      )}
     </div>
   );
 };
