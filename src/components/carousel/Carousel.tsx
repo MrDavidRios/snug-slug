5d138f99d379af8b755e4b2c9272048df9711f48
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-11-20 22:33:01 -0500

    feat: Swiper (carousel package) added

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface CarouselProps {}
+
+const Carousel: React.FC<CarouselProps> = () => {
+  return (
+    // Add your JSX code here
+    <div>{/* Add your component content here */}</div>
+  );
+};
+
+export default Carousel;
