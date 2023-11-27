661b97c534830b5896ad574cfb8332e1288bcda2
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-11-27 11:59:17 -0500

    flask API up and running

---
+++ backend/README.md
@@ -0,0 +1,13 @@
+## Setup
+
+Make sure to get a virtual environment up and running.
+
+1. Run `python -m venv venv`
+2. Run `source venv/bin/activate`. If on Windows, run `venv/Scripts/activate` instead.
+3. You should now be in your virtual environment. Run `pip install -r requirements.txt` to install all dependencies.
+
+You're good to go!
+
+## Running the server
+
+Simply run `flask run` in this directory (`./backend`). It should start on `localhost:5000`.
--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,12 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS # comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/dist')
-CORS(app) # comment this on deployment
+app = Flask(__name__, static_url_path='/../', static_folder='/frontend/dist')
+# CORS(app) # comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(HelloApiHandler, '/flask/hello')
