f828c860c4aa04c9232e51a0c5a787ed5d925fbb
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-11-27 14:32:39 -0500

    feat: login page implemented

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -1,23 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SnugSlugLogo from "../../assets/SnugSlugLogo.png";
+import SnugSlugLogo from "../../assets/Logo.png";
 import { Size } from "../../types/size";
 
 interface HeroProps {
   redirectToHome?: boolean;
   size?: Size;
+  vertical?: boolean;
 }
 
-export const Hero: React.FC<HeroProps> = ({ redirectToHome = false, size = Size.Large }) => {
+export const Hero: React.FC<HeroProps> = ({ redirectToHome = false, size = Size.Large, vertical = false }) => {
   return (
-    <div className={`hero size-${size}`}>
+    <>
       {redirectToHome ? (
-        <Link to="/">
-          <img src={SnugSlugLogo} alt="Logo" />
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          <HeroContents size={size} vertical={vertical} />
         </Link>
       ) : (
-        <img src={SnugSlugLogo} alt="Logo" />
+        <HeroContents size={size} vertical={vertical} />
       )}
+    </>
+  );
+};
+
+const HeroContents: React.FC<{ size?: Size; vertical?: boolean }> = ({ size = Size.Large, vertical = false }) => {
+  return (
+    <div className={`hero size-${size} ${vertical ? "vertical" : ""}`}>
+      <img src={SnugSlugLogo} alt="Logo" />
+
+      {/* Different heading sizes */}
+      {size === Size.Large && <h1>Snug Slug</h1>}
+      {size === Size.Medium && <h2>Snug Slug</h2>}
+      {size === Size.Small && <h3>Snug Slug</h3>}
     </div>
   );
 };
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,17 +1,24 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import React from "react";
+import googleIcon from "../../assets/google-logo.svg";
 import { Hero } from "../../components/hero/Hero";
 
 export const Login: React.FC = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const signInWithGoogle = () => signInWithPopup(auth, provider);
+
   return (
     <div id="loginPageWrapper">
-      <Hero />
-      <h2>Login</h2>
-      {/* This is going to have to change (i think? Not sure how to implement forms yet) */}
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      <Hero vertical={true} />
+      <p className="italics" style={{ padding: 10 }}>
+        The trusted subleasing platform for our Columbia University community.
+      </p>
+      <button onClick={signInWithGoogle} id="googleSignInBtn">
+        <img src={googleIcon} />
+        <p>Sign in with Google</p>
+      </button>
     </div>
   );
 };
