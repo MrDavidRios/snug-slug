a87372b1d0298aab0c9c3f5d157d50f7e0fd570e
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-11-28 12:10:40 -0500

    feat: marketplace test page added

--- frontend/src/pages/marketplace/Marketplace.tsx
+++ frontend/src/pages/marketplace/Marketplace.tsx
@@ -1,8 +1,51 @@
 import { useState } from "react";
+import { ListingsView } from "../../components/cardList/cardList";
 import { DetailedListing } from "../../components/detailedListing/DetailedListing";
+import { Listing } from "../../types/listing";
+
+const sampleListings: Listing[] = [
+  {
+    location: "76th Street, Upper West Side",
+    description: "1 Bedroom in a 3 Bedroom suite",
+    features: ["In-unit washer", "Dog friendly"],
+    dates: "May 15 - Aug 31",
+    rent: 1500,
+    apartmentImgUrl: "src/assets/apartment.png",
+  },
+  {
+    location: "76th Street, Upper West Side",
+    description: "1 Bedroom in a 3 Bedroom suite",
+    features: ["In-unit washer", "Dog friendly"],
+    dates: "May 15 - Aug 31",
+    rent: 1500,
+    apartmentImgUrl: "src/assets/apartment.png",
+  },
+  {
+    location: "76th Street, Upper West Side",
+    description: "1 Bedroom in a 3 Bedroom suite",
+    features: ["In-unit washer", "Dog friendly"],
+    dates: "May 15 - Aug 31",
+    rent: 1500,
+    apartmentImgUrl: "src/assets/apartment.png",
+  },
+  {
+    location: "76th Street, Upper West Side",
+    description: "1 Bedroom in a 3 Bedroom suite",
+    features: ["In-unit washer", "Dog friendly"],
+    dates: "May 15 - Aug 31",
+    rent: 1500,
+    apartmentImgUrl: "src/assets/apartment.png",
+  },
+];
 
 export const Marketplace: React.FC = () => {
   const [showDetailedListing, setShowDetailedListing] = useState(false);
 
-  return <div id="marketplacePageWrapper">{showDetailedListing && <DetailedListing />}</div>;
+  return (
+    <div id="marketplacePageWrapper">
+      <ListingsView listings={sampleListings} />
+      {showDetailedListing && <DetailedListing listing={sampleListings[0]} />}
+      <button onClick={() => setShowDetailedListing(true)}>Test show detailed listing</button>
+    </div>
+  );
 };
