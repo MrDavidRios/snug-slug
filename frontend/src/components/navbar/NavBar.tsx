731dc62b6a44f82d202be672897fb9c77c8a6a4c
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-12-05 22:02:48 -0500

    feat: button interaction improved

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -1,10 +1,18 @@
 import { Size } from "../../types/size";
+import { Button } from "../button/Button";
 import { Hero } from "../hero/Hero";
 
 export const Nav: React.FC = () => {
   return (
     <nav>
       <Hero size={Size.Small} redirectToHome={true} />
+      <div className="nav-buttons">
+        <Button className="secondary" text="FAQ" onClick={() => {}} />
+        <Button className="secondary" text="Listing" onClick={() => {}} />
+        <Button className="secondary" text="Chats" onClick={() => {}} />
+        <Button className="secondary" text="Saved" onClick={() => {}} />
+        <Button className="action" text="Profile" onClick={() => {}} />
+      </div>
     </nav>
   );
 };
