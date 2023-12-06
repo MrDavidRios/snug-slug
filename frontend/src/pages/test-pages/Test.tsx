83eb201cbe4cdf7ee57c8ed60b8b272d41f68061
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-12-05 21:45:20 -0500

    fix: test case removed

--- frontend/src/pages/test-pages/Test.tsx
+++ frontend/src/pages/test-pages/Test.tsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { ListingCard } from "../../components/apartmentCard/ListingCard";
 import { Carousel } from "../../components/carousel/Carousel";
 import { Dropdown } from "../../components/dropdown/Dropdown";
 import { Modal } from "../../components/modal/Modal";
-import { Listing } from "../../types/listing";
-
-const testListing: Listing = {
-  location: "1234 Test Street",
-  overview: "This is a test listing",
-  details: ["This is a test listing"],
-  requirements: ["This is a test listing"],
-  additionalInfo: ["This is a test listing"],
-  tags: ["tag1", "tag2", "tag3"],
-  dates: "01/01/2021 - 01/01/2022",
-  rent: 1000,
-  apartmentImgUrls: [
-    "https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU",
-  ],
-};
 
 /* Carousel Test Code */
 const testApartmentImgUrls = [
@@ -32,12 +16,7 @@ export const Test: React.FC = () => {
   return (
     <div>
       <Carousel imgUrls={testApartmentImgUrls} />
-      <Dropdown
-        options={["Option 1", "Option 2", "Option 3"]}
-        placeholder="Select an Option"
-        onChange={() => {}}
-      />
-      <ListingCard listing={testListing} locationIndex={0} />
+      <Dropdown options={["Option 1", "Option 2", "Option 3"]} placeholder="Select an Option" onChange={() => {}} />
       {showModal && (
         <Modal title="Test" onClose={() => setShowModal(false)}>
           Hey there!
