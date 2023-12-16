d043b9db092117f1dd43e7e6e5b24315771da703
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-12-16 15:11:21 -0500

    feat: added login feedback

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import googleIcon from "../../assets/google-logo.svg";
 import { UserContext, UserContextType } from "../../components/UserContext";
+import { Button } from "../../components/button/Button";
 import { Hero } from "../../components/hero/Hero";
 import { exampleUserA } from "../../utils/inboxtestdata";
 
@@ -8,7 +10,7 @@ export const Login: React.FC = () => {
   // const provider = new GoogleAuthProvider();
   // const auth = getAuth();
 
-  const { setSlug } = useContext(UserContext) as UserContextType;
+  const { slug, setSlug } = useContext(UserContext) as UserContextType;
 
   const signInWithGoogle = () => {
     //signInWithPopup(auth, provider)
@@ -19,14 +21,25 @@ export const Login: React.FC = () => {
 
   return (
     <div id="loginPageWrapper">
-      <Hero vertical={true} />
-      <p className="italics" style={{ padding: 10 }}>
-        The trusted subleasing platform for our Columbia University community.
-      </p>
-      <button onClick={signInWithGoogle} id="googleSignInBtn">
-        <img src={googleIcon} />
-        <p>Sign in with Google</p>
-      </button>
+      {slug ? (
+        <>
+          <h1>Logged in!</h1>
+          <Link to="/">
+            <Button className="action" text="Homepage" />
+          </Link>
+        </>
+      ) : (
+        <>
+          <Hero vertical={true} />
+          <p className="italics" style={{ padding: 10 }}>
+            The trusted subleasing platform for our Columbia University community.
+          </p>
+          <button onClick={signInWithGoogle} id="googleSignInBtn">
+            <img src={googleIcon} />
+            <p>Sign in with Google</p>
+          </button>
+        </>
+      )}
     </div>
   );
 };
